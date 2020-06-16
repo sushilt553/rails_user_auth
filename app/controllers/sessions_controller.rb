08834534e565a2fba236410cd4a7fce5cf4710b0
Sushil <sushilt553@gmail.com>
2020-06-16 13:39:49 -0400

    render in application.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,30 @@
 class SessionsController < ApplicationController
 
     def new
+        render :new
     end
 
     def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if @user
+            login(@user)
+            render :show
+        else
+            render json: ["Username/Password didn't match"], status: :unprocessable_entity
+        end
     end
 
     def destroy
+        self.logout
+        redirect_to new_session_url
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
         if @user.save
             login(@user)
+            render :show
         else
             render json: @user.errors.full_messages, status: :unprocessable_entity
         end
