930308691fe500d62162f0adf3d81bc92fd2df24
Sushil <sushilt553@gmail.com>
2020-06-16 12:29:46 -0400

    password= method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
     validates :session_token, presence: true
     validates :password, length: {minimum: 6, allow_nil: true}
 
+    after_initialize :ensure_session_token
+    attr_reader :password
+
     def self.find_by_credentials(username, password) 
         user = User.find_by(username: username)
         user && user.is_password?(password) ? user : nil
@@ -12,5 +15,24 @@ class User < ApplicationRecord
 
     def self.generate_session_token
         SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        self.session_token = self.class.generate_session_token
+        self.session_token
+    end
+
+    def is_password?(password) 
+    end
+
+    def password=(password) 
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    private
+
+    def ensure_session_token
+        self.session_token ||= self.class.generate_session_token
     end
 end
