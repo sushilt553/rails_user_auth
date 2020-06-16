739db01959e593fbe9d9870abfacae1d995236f8
Sushil <sushilt553@gmail.com>
2020-06-16 12:22:30 -0400

    User model level validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :session_token, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+
+    
 end
