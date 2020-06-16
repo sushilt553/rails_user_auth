e910e0e08446ffbcd88b204222aa30ac13716050
Sushil <sushilt553@gmail.com>
2020-06-16 13:18:49 -0400

    Login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,17 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
+
+        if @user.save
+            login(@user)
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
     end
 end
