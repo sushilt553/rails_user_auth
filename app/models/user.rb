86e1573f96bf656dfc66466e9c20f1a1845f5234
Sushil <sushilt553@gmail.com>
2020-06-16 12:25:03 -0400

    find-by-credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,12 @@ class User < ApplicationRecord
     validates :session_token, presence: true
     validates :password, length: {minimum: 6, allow_nil: true}
 
-    
+    def self.find_by_credentials(username, password) 
+        user = User.find_by(username: username)
+        user && user.is_password?(password) ? user : nil
+    end
+
+    def self.generate_session_token
+
+    end
 end
